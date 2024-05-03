60bec9b96f01ff68cd23e07a58ace35f19b86bbf
Vince Pag-ong <vince.ookaay@gmail.com>
2024-05-03 10:12:53 +0800

    dark mode draft

--- scripts/themeChanger.js
+++ scripts/themeChanger.js
@@ -1,20 +1,44 @@
 const modeButton = document.querySelector("#mode");
-const moon = document.querySelector("#darkBtn");
-const sun = document.querySelector("#lightBtn");
+const moon = document.querySelector(".darkBtn");
+const sun = document.querySelector(".lightBtn");
 const body = document.querySelector("body");
+const header = document.querySelector("header");
+const profilPicture = document.querySelector("#profil-picture");
+const mainSection = document.querySelector("main section");
+const nav = document.querySelector("nav");
+const card = document.querySelectorAll(".card");
+const h1 = document.querySelectorAll("h1");
+const active = document.querySelector(".active");
+const cardULAV = document.querySelectorAll(".card ul li a:visited");
+const h2 = document.querySelectorAll("h2");
+const menu = document.querySelectorAll("#menu");
+const footer = document.querySelector("footer");
+
+
 
 modeButton.addEventListener("click", () => {
-    if (moon.style.display === "none") {
-        // If moon image is hidden, show moon and hide sun
-        moon.style.display = "inline";
-        sun.style.display = "none";
-        body.style.background = "#eee";
-        body.style.color = "#000";
-    } else {
-        // If sun image is hidden, show sun and hide moon
-        moon.style.display = "none";
-        sun.style.display = "inline";
-        body.style.background = "#000";
-        body.style.color = "#fff";
-    }
+    moon.style.display = moon.style.display === "none" ? "inline" : "none";
+    sun.style.display = sun.style.display === "none" ? "inline" : "none";
+    body.classList.toggle("dark");
+    header.classList.toggle("dark");
+    profilPicture.classList.toggle("dark");
+    nav.classList.toggle("dark");
+    active.classList.toggle("dark");
+    mainSection.classList.toggle("dark");
+    card.forEach(element => {
+        element.classList.toggle("dark");
+    });
+    h1.forEach(element => {
+        element.classList.toggle("dark");
+    });
+    cardULAV.forEach(element => {
+        element.classList.toggle("dark");
+    });
+    h2.forEach(element => {
+        element.classList.toggle("dark");
+    });
+    menu.forEach(element => {
+        element.classList.toggle("dark");
+    });
+    footer.classList.toggle("dark");
 });
