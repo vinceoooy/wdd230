26281aa3fd5d64c9fb8d1c7304b6ec7122cb64f5
Vince Pag-ong <vince.ookaay@gmail.com>
2024-05-06 04:05:36 +0800

    Lazy load

---
+++ week03/scripts/getDates.js
@@ -0,0 +1,5 @@
+//set copyright year
+document.getElementById("copyrightYear").textContent = new Date().getFullYear();
+
+//Fetch last modified date of the document
+document.getElementById("lastModified").textContent = document.lastModified;
