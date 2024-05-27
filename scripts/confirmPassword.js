25b2256732005b655c35ff8374bbe29f5bd776ea
Vince Pag-ong <vince.ookaay@gmail.com>
2024-05-28 05:11:23 +0800

    password

--- scripts/confirmPassword.js
+++ scripts/confirmPassword.js
@@ -2,10 +2,12 @@ const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm_password');
 
 confirmPassword.addEventListener('focusout', checkSame);
+password.addEventListener('focusout', checkSame);
 
 function checkSame() {
     if (password.value !== confirmPassword.value) {
         confirmPassword.style.backgroundColor = "#fff0f3";
+        confirmPassword.value="";
     }
     else {
 		confirmPassword.style.backgroundColor = "#fff";
